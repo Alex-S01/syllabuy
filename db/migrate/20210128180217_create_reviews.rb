f78dd63bb68e2abb5a557b9678cd3087ad2d7952
sebT-max <sebternest@hotmail.com>
2021-01-28 20:58:42 +0100

    brol

--- db/migrate/20210128180217_create_reviews.rb
+++ db/migrate/20210128180217_create_reviews.rb
@@ -1,5 +1,14 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
+     create_table :purchases do |t|
+      t.string :payment_method
+      t.date :purchase_date_time
+      t.date :closing_date_time
+      t.references :book, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
     create_table :reviews do |t|
       t.integer :rating
       t.string :content
