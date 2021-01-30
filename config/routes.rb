9e89dff978a636b5711da8e864bd9252bf442f10
Lucas Goretti <lucas@cowboy.com>
2021-01-30 12:54:59 +0100

    purchase index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :purchases, only: [:index]
+  end
+  resources :books do
+    resources :purchases, only: [:new, :create]
+  end
+  resources :purchases, only: [:index, :show]
 end
