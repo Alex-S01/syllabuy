cacdc88f76dedd285e97b124cea5613f537996b5
Lucas Goretti <lucas@cowboy.com>
2021-01-30 14:12:22 +0100

    done

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,4 +7,16 @@ class PurchasesController < ApplicationController
       @purchase = Purchase.find(params[:id])
   end
 
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @book = Book.find(params[:book_id])
+    @purchase = Purchase.new
+  end
+
+end
+
+
+
+
+
 end
