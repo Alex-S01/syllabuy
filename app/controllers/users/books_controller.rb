dd52641237886aed053085b94c50fa29638685b4
Alex Saliba <alexanderpsaliba@gmail.com>
2021-02-09 20:39:55 +0100

    My books shows only books with status false

--- app/controllers/users/books_controller.rb
+++ app/controllers/users/books_controller.rb
@@ -1,7 +1,7 @@
 class Users::BooksController < ApplicationController
   def index
     user = User.find(params[:user_id])
-    @books = user.books
+    @books = user.books.where(status: "false")
 
     render 'books/my_books'
   end
