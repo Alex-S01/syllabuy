2aaba2421317a57f23c7b25e1eddd85957e35598
Alex Saliba <alexanderpsaliba@gmail.com>
2021-02-13 11:57:27 +0100

    edit profile devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   # [...]
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :about_me, :gender, :phone_number, :university, :year_of_study, :date_of_birth])
+  end
 
 end
