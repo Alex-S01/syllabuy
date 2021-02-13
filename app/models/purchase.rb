b8bb15caaeb0edd860ba28b50a6169c86bae7308
Alex Saliba <alexanderpsaliba@gmail.com>
2021-02-13 20:06:33 +0100

    added uniqueness to review a purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,8 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  validates :payment_method, presence: true
   has_one :review, dependent: :destroy
+
+  validates :payment_method, presence: true
+
 end
