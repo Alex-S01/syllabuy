94a3ae8be12173498060a56763f48d6ec011e4c0
Lucas Goretti <lucas@cowboy.com>
2021-02-19 00:05:21 +0100

    photo swapped

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,8 +1,6 @@
 import mapboxgl from 'mapbox-gl';
-
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
@@ -18,30 +16,25 @@ const initMapbox = () => {
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(new mapboxgl.Popup({ offset: 25}) // add popups
-        .setHTML(marker.infoWindow))
+        .setHTML(marker.infoWindow.content))
         .addTo(map);
     });
   fitMapToMarkers(map, markers);
   }
 };
-
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5000 });
 };
-
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
-
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup) // add this
       .addTo(map);
   });
 };
-
 export { initMapbox };
 export { addMarkersToMap };
-
