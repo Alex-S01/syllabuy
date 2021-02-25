4d0c664a9027342f8040137718ceed28475c0cc4
Alex Saliba <alexanderpsaliba@gmail.com>
2021-02-25 23:41:05 +0100

    added meta tags to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :about_me, :gender, :phone_number, :university, :year_of_study, :date_of_birth, :photo])
   end
 
+  # setting up meta tags for our application
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
 end
