ff9deac08deb8141558885cc341e3f6d595509c0
Alex Saliba <alexanderpsaliba@gmail.com>
2021-02-27 11:42:00 +0100

    fix zoom to 7 on map books

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -25,7 +25,7 @@ const initMapbox = () => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 10, duration: 4000 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 7, duration: 4000 });
 };
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
